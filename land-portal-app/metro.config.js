1226429604f99573b471c289669fba903583d616
Yeab Mari <yeabmari@gmail.com>
2026-05-27 08:37:56 +0300

    updated project

--- land-portal-app/metro.config.js
+++ land-portal-app/metro.config.js
@@ -1,8 +1,17 @@
 const path = require("path");
 
-// Use only this app's Babel config (ignore parent repo babel.config.js).
-process.env.BABEL_CONFIG = path.join(__dirname, "babel.config.js");
+const projectRoot = __dirname;
+
+// Isolate this app from the parent repo (fixes babel-preset-expo / Metro on Windows).
+process.env.BABEL_CONFIG = path.join(projectRoot, "babel.config.js");
+process.env.EXPO_NO_METRO_WORKSPACE_ROOT = "1";
 
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(projectRoot);
+
+// Only resolve modules from this app (not parent landd/node_modules).
+config.watchFolders = [projectRoot];
+config.resolver.nodeModulesPaths = [path.join(projectRoot, "node_modules")];
+
+module.exports = config;
